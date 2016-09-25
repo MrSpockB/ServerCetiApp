70169d30003d0fb34b14694e9b3d5a7e71ef21be
Ivan Barragan <idsivanbarragan@yahoo.com>
2016-09-24 19:57:42 -0500

    Controlador de mensajes
    funciones para traer todos y de una sola conversacion

---
+++ app/controllers/mensajes.js
@@ -0,0 +1,32 @@
+var Mensajes = require('./../models/mensaje');
+
+module.exports = {
+	index: 
+	 {
+	 	//esta funcion trae todos los mensajes
+	 	post: function(req, res, next){
+	 		new Mensajes({
+	 			conversacion_id: req.body.idConversacion,
+	 			texto: req.body.texto,
+	 			remitente_id: req.body.idremitente
+	 		}).save().then(function(mensaje){
+	 			res.json(mensaje);
+	 		}).catch(function(err){
+	 			res.json(err);
+			})
+	 	}
+	 },
+	
+	"conv/:id":
+	{
+		get: function(req, res, next){
+			//conversacion_id es un campo de la tabla mensaje, por la que voy a hacer la consulta "traeme todo donde conversacion_id sea igual a id"
+			 Mensajes.query('where', 'conversacion_id', '=', req.params.id).fetchAll().then(function(mensajes){
+	 			res.json(mensajes);
+	 		}).catch(function(err){
+	 			res.json(err);
+			})
+	 	},
+
+	}
+};

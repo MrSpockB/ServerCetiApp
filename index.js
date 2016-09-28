c66a9bdcfab455694fb54edaf61f1055f5e67938
alfredopizana <pizana.alfredo@gmail.com>
2016-09-27 20:16:38 -0500

    Chat Agregado

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ var config = require('./app/config/config'),
 
 var app = express();
 
-app.listen(config.port);
+var http = require('http');
+var server = app.listen(config.port);
+var io = require('socket.io').listen(server);
+
+//var io = require('socket.io')(http);
+//console.log(io);
+
+
+
+//io.listen(http);
+
+
+
+
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('chat message', function(msg){
+	    console.log('message: ' + msg);
+	    io.emit('chat message', msg);
+	  });
+    socket.on('event', function(data){
+
+    });
+    socket.on('disconnect', function(){
+
+    });
+});
 
 console.log(process.env.NODE_ENV  + ' server running at http://localhost:' + config.port);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname +'/hello.html');
+});

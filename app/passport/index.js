09dfdeb127fe3ac75658468b4fdcb64bd9ac665a
alfredopizana <pizana.alfredo@gmail.com>
2016-11-25 21:26:02 -0600

    Minor Fixes

--- app/passport/index.js
+++ app/passport/index.js
@@ -4,11 +4,10 @@ module.exports = function(passport)
 {
 	var localLoginStrategy = require('./local-login')(config);
 	var localSignupStrategy = require('./local-signup')(config);
-	//var localRequestCodeStrategy = require('./local-request-code')(config);
 	
 	passport.use('local-signup', localSignupStrategy);
 	passport.use('local-login', localLoginStrategy);
-	//passport.use('local-request-code', localLoginStrategy);
+
 
 
 };
